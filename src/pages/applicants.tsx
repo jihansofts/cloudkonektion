1bad1cb9a9bda7fea87df08ba9c7cb0721b1ebcc
skrifatuiux <skrifatuiux@gmail.com>
2025-09-11 17:33:37 +0600

    update

--- src/pages/applicants.tsx
+++ src/pages/applicants.tsx
@@ -13,6 +13,7 @@ export default function applicants() {
       <Consultations />
       <Improve />
       <Personality />
+      
     </main>
   );
 }
--- src/pages/employers.tsx
+++ src/pages/employers.tsx
@@ -25,6 +25,7 @@ export default function employers() {
         imageAlt="Bob"
       />
       <Personality />
+      <Banner />
     </main>
   );
 }
